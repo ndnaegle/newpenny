1da53a4be3e5672f30d969ae38c82ec8b52e0c24
Jon Thomason <Jon.Thomason@Oculus.com>
2017-06-24 13:15:58 -0700

    blinking digit patterns

--- blink/blink.ino
+++ blink/blink.ino
@@ -10,17 +10,34 @@
  */
 
 int ledPin = 13;                // LED connected to digital pin 13
+int loopCount = 0;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin, OUTPUT);      // sets the digital pin as output
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-  delay(100);
+//  digitalWrite(ledPin, HIGH);
+//  delay(100);
+//  digitalWrite(ledPin, LOW);
+//  delay(100);
+//  Serial.print("Finished a loop #");
+//  Serial.println(++loopCount, DEC);
+   blinkNtimes(5);
+   delay(1000);
+   blinkNtimes(3);
+   delay(2000);
+}
+
+void blinkNtimes(int n) {
+  for (int i = 0 ; i < n ; ++i) {
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+    digitalWrite(ledPin, LOW);
+    delay(200);    
+  }
 }
 
